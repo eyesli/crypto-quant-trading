4a93f6b8d900724e4b571e1aadddb60026e1c961
levi li <453321481@qq.com>
2025-12-11 19:49:06 +0800

    add

--- main.py
+++ main.py
@@ -4,23 +4,25 @@
 """
 
 import sys
-
+import time
 from src.exchange_manager import create_exchange
 from src.service import start
 
 
 if __name__ == "__main__":
-    try:
-        exchange = create_exchange()
 
-        start(exchange)
-    except KeyboardInterrupt:
-        print("\n\n⚠️  用户中断程序")
-        sys.exit(0)
-    except Exception as e:
-        print(f"\n❌ 程序执行出错: {e}")
-        import traceback
 
-        traceback.print_exc()
-        sys.exit(1)
+        try:
+            exchange = create_exchange()
+            while True:
+                start(exchange)
+                time.sleep(60)  # 每分钟运行一次
+        except KeyboardInterrupt:
+            print("\n\n⚠️  用户中断程序")
+            sys.exit(0)
+        except Exception as e:
+            print(f"\n❌ 程序执行出错: {e}")
+            import traceback
+            traceback.print_exc()
+
 
